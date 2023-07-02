a2d95c24a45778a8228de78aaf65f2c0af529020
Denys Tereshchuk <terrya@ukr.net>
2023-07-02 21:08:39 +0400

    Updated views.py, index.html & added form.html

--- generator/views.py
+++ generator/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from generator.forms import GeneratorForm
+
 
 @login_required
 def index(request):
-    return render(request, template_name="index.html")
+    form = GeneratorForm()
+
+    return render(request, template_name="index.html", context={"form": form})
