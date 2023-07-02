7c71ccf88c5d1c95b01fc22daed9eb637da64f66
Denys Tereshchuk <terrya@ukr.net>
2023-07-02 15:41:15 +0400

    Updated admin panel for user

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
-# Register your models here.
+from user.models import User
+
+
+admin.site.register(User)
+admin.site.unregister(Group)
