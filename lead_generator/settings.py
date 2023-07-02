a26e2d06056f2345c43c1acb8a71cd974c8facfb
Denys Tereshchuk <terrya@ukr.net>
2023-07-02 16:38:25 +0400

    Added bootstrap & crispy forms

--- lead_generator/settings.py
+++ lead_generator/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "generator",
     "user",
 ]
@@ -70,6 +72,10 @@ TEMPLATES = [
         },
     },
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 WSGI_APPLICATION = "lead_generator.wsgi.application"
 
